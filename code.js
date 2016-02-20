5bcf504b556ab12311fdb34f426e82a726d348a2
Pablo Martinez <pmartinez@mixpo.com>
2016-02-20 09:01:46 -0800

    initial commit

---
+++ code.js
@@ -0,0 +1,63 @@
+function press(){
+	$('#modal').removeClass('modal-hide');
+	$('#modal').addClass('modal-show');
+}
+function hideModal(){
+	$('#modal').addClass('modal-hide');
+	$('#modal').removeClass('modal-show')
+}
+var valid = 0;
+var removeSpecialCharacters = "";
+function validUser(){
+	if($('input[name=user]').val() == ""){
+		console.log('user');
+		$('#user').html('Please enter a user name');
+	}
+	else {
+		console.log('good user');
+		valid++;
+	}
+}
+ function validAddress(){
+	 var addressEntered = $('input[name=address]').val();
+	 if (addressEntered == "") {
+		console.log('address');
+		$('#address').html('Please enter valid address (no special characters)')
+	}
+	else{
+		console.log('good address');
+		removeSpecialCharacters = addressEntered.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
+		valid++;
+	}
+ }
+ function validDescription(){
+  if ($('input[name=description]').val() == "") {
+ 	console.log('description');
+ 	$('#description').html('Please enter description')
+ }
+ else{
+ 	console.log('good description');
+ 	valid++
+ }
+ }
+ function validMural(){
+	 if ($('input[name=mural]').val() == "") {
+		console.log('mural');
+		$('#mural').html('Please enter a mural name')
+	}
+	else{
+		console.log('good mural');
+		valid++;
+	}
+ }
+function validatedForm(){
+		validUser();
+		validAddress();
+		validDescription();
+		validMural();
+		console.log(valid);
+		if(valid == 4){
+			hideModal();
+		}
+		valid = 0;
+}
