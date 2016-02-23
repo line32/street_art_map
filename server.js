0cd79fb8e990f926aa14e2614f6a63951f59ffe5
Frederic Viennot <fviennot@kcts9.org>
2016-02-22 19:23:37 -0800

    comment out config

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var config = require('./config') || null;
+//var config = require('./config') || null;
 console.log(config);
 var DB = process.env.DB || config.DB;
 var PORT = process.env.PORT || config.PORT;
