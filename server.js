43c64fbeb639dc20191df4dacf490b8dcf5b04bd
Frederic Viennot <fviennot@kcts9.org>
2016-02-22 19:41:14 -0800

    headers in the right place

--- server.js
+++ server.js
@@ -10,13 +10,15 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static('.'));
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.use(express.static('.'));
+
 
 app.get('/art', function(req, res) {
   models.Art.findAll().then(function(art) {
