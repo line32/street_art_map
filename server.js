ac0504432dcf9edaf85cb4f942988e058844e79a
Frederic Viennot <fviennot@kcts9.org>
2016-02-22 19:47:13 -0800

    /art in app.all

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('.'));
 
-app.all('/', function(req, res, next) {
+app.all('/art', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
