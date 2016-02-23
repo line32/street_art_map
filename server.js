55dd4a333a3cf1e5d0beaca059a01fcf4e12ceb4
Frederic Viennot <fviennot@kcts9.org>
2016-02-22 18:45:25 -0800

    move server.js and config.js to root

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();	
+  next();
 });
 
 app.get('/art', function(req, res) {
