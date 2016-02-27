4a9239be5bb7078dd0d2e4d36a6e57a5bbe95fca
Pablo Martinez <pmartinez@mixpo.com>
2016-02-26 21:29:14 -0800

    all data is rendering now

--- scripts/encode.js
+++ scripts/encode.js
@@ -3,8 +3,8 @@ $.getJSON('https://seattleartmap.herokuapp.com/art', function( data ) {
     //console.log(data);
   //var image = data[2].image;
   //console.log(image);
-  console.log(image);
+  // console.log(image);
   //var decodedimg = atob(image);
   //document.getElementById("stuff").src = image;
-  console.log(decodedimg);
+  // console.log(decodedimg);
 });
